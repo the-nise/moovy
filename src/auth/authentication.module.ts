d9d78ff6dba020cfdd63dbb689ea72d23c18e45c
the-nise <64296151+the-nise@users.noreply.github.com>
2022-12-16 10:02:28 -0300

    Hid keys

--- src/auth/authentication.module.ts
+++ src/auth/authentication.module.ts
@@ -3,15 +3,16 @@ import { AuthService } from './authentication.service';
 import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './authentication.constants';
 import { LocalStrategy } from './strategies/local.strategy';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
   imports: [
     UserModule,
     PassportModule,
     JwtModule.register({
-      secret: jwtConstants.secret,
+      secret: process.env.SECRET,
       signOptions: { expiresIn: '60s' },
     }),
   ],
